a6faa79206a27540730e3fec91480970428cfd70
Sangwa-C <chelseasabinesangwa@gmail.com>
2019-10-26 11:40:04 +0200

    create choiceActivity

---
+++ app/src/main/java/com/example/ihahire/ui/ChoiceActivity.java
@@ -0,0 +1,50 @@
+package com.example.ihahire.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.ihahire.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class ChoiceActivity extends AppCompatActivity {
+    @BindView(R.id.buy)
+    Button mBuy;
+    @BindView(R.id.sell) Button mSell;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choice);
+
+        ButterKnife.bind(this);
+
+        mBuy.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChoiceActivity.this, BuyMainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
+        mSell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1=new Intent(ChoiceActivity.this, sellActivity.class);
+                startActivity(intent1);
+            }
+        });
+
+
+
+
+    }
+}
